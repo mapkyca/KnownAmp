2315d5eec2a0b2731550cc82e1734340756f996b
Marcus Povey <marcus@marcus-povey.co.uk>
2018-03-28 10:08:18 +0100

    Moving AMP views from core

---
+++ Amp/Main.php
@@ -0,0 +1,19 @@
+<?php
+
+    namespace IdnoPlugins\Amp {
+
+        class Main extends \Idno\Common\Plugin {
+            function registerPages() {
+
+            }
+
+            function registerContentTypes() {
+
+            }
+            
+            function registerTranslations() {
+
+            }
+        }
+
+    }
